2ec64f5a589084bbb5d913abd66d559b4471f32f
Your Name <tauhid.swe@gmail.com>
2023-11-09 11:51:42 +0600

    make the banner beautiful

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
     return (
         <div
             className='d-flex flex-column flex-md-row justify-content-around align-items-center bg-info w-100'
-            style={{ padding:'40px' }}
+            style={{ padding:'20px 30px 20px 30px' }}
         >
             <div className='fs-1 fw-bold text-light text-center text-uppercase text-shadow px-3'>Smart Transformer <br /> Monitoring System</div>
             <img src={banImg} className='w-50' alt="Banner Image"/>
