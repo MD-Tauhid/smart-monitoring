66fa44b0c2f141957145be669fcbc80a2d637a8b
Your Name <tauhid.swe@gmail.com>
2023-11-09 14:54:17 +0600

    for image fade

--- src/Components/Monitoring/Monitoring1.jsx
+++ src/Components/Monitoring/Monitoring1.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Monitoring1 = ({ img, text, bg, shadow }) => {
     return (
         <div className={`w-100 d-flex flex-column-reverse flex-md-row-reverse justify-content-center justify-content-md-between align-items-center my-6 ${bg}`} style={{padding:'50px'}}>
-            <img data-aos="fade-left" data-aos-duration="2000" className={`w-50 ${shadow}`} src={img} alt="" />
+            <img data-aos-duration="2000" className={`w-50 ${shadow}`} src={img} alt="" />
             <div className='mx-auto'>
-                <h1 data-aos-duration="2000" className='py-3 text-center text-shadow' style={{maxWidth:'400px'}} >{text}</h1>
+                <h1 data-aos="fade-right" data-aos-duration="2000" className='py-3 text-center text-shadow' style={{maxWidth:'400px'}} >{text}</h1>
             </div>
         </div>
     );
