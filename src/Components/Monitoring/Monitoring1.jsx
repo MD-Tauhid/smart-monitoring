d3cb47be4aca784da6f726b69ca33f81d4f6b922
Your Name <tauhid.swe@gmail.com>
2023-11-09 14:49:08 +0600

    Changed

--- src/Components/Monitoring/Monitoring1.jsx
+++ src/Components/Monitoring/Monitoring1.jsx
@@ -5,7 +5,7 @@ const Monitoring1 = ({ img, text, bg, shadow }) => {
         <div className={`w-100 d-flex flex-column-reverse flex-md-row-reverse justify-content-center justify-content-md-between align-items-center my-6 ${bg}`} style={{padding:'50px'}}>
             <img data-aos="fade-left" data-aos-duration="2000" className={`w-50 ${shadow}`} src={img} alt="" />
             <div className='mx-auto'>
-                <h1 data-aos="fade-right" data-aos-duration="2000" className='py-3 text-center text-shadow' style={{maxWidth:'400px'}} >{text}</h1>
+                <h1 data-aos-duration="2000" className='py-3 text-center text-shadow' style={{maxWidth:'400px'}} >{text}</h1>
             </div>
         </div>
     );
