02ca07f63eb354595bb7d96815f66b3103f3a04b
Your Name <tauhid.swe@gmail.com>
2023-11-09 12:44:16 +0600

    done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -18,10 +18,10 @@ const Home = () => {
             <div data-aos="fade-up" data-aos-duration="3000" className='d-flex flex-column align-items-center p-5'>
                 <img className='h-25 w-50 mx-auto ' src={img1} alt="" style={{width:'60%'}} />
             </div>
-            <section className='bg-light d-flex flex-column flex-md-row-reverse justify-content-md-around align-items-center fs-1 fw-bold p-5'>
-                <div className='d-flex flex-column justify-content-start justify-content-md-around  mb-3'>
-                    <div className='mx-auto text-center text-warning fs-3'>Measure Items<br />(Phase And Line Parameter)</div>
-                    <div className='d-flex flex-row justify-content-start justify-content-md-around fs-5'>
+            <section className='bg-light d-flex flex-column flex-md-row justify-content-start justify-content-md-around align-items-center fs-1 fw-bold p-5'>
+                <div className='d-flex flex-column justify-content-start mb-3'>
+                    <div className='text-warning fs-2'>Measure Items<br />(Phase And Line Parameter)</div>
+                    <div className='d-flex flex-row justify-content-start fs-5'>
                         <ul style={{ listStyle: 'circle outside' }}>
                             <li>Current</li>
                             <li>Voltage</li>
@@ -33,10 +33,10 @@ const Home = () => {
                         </ul>
                     </div>
                 </div>
-                <div className='d-flex flex-column justify-content-start justify-content-md-around'>
-                    <div className='mx-auto text-warning fs-3'>Safety Monitoring</div>
-                    <div className='d-flex flex-row justify-content-start justify-content-md-around fs-5'>
-                        <ul>
+                <div className='d-flex flex-column justify-content-start'>
+                    <div className='text-warning fs-2'>Safety Monitoring</div>
+                    <div className='d-flex flex-row justify-content-start fs-5'>
+                        <ul style={{ listStyle: 'square' }}>
                             <li>Phase Loss or Reovery</li>
                             <li>Oil Level or Temperature</li>
                             <li>OverLoad or Exeed pre-set Value</li>
@@ -46,7 +46,7 @@ const Home = () => {
                 </div>
             </section>
             <section className='d-flex flex-column justify-content-center mx-auto fs-1 fw-bold pt-5'>
-                <div className='text-uppercase text-info mx-md-auto mb-5 text-center text-shadow'>Remote Monitoring & Report System</div>
+                <div className='text-uppercase text-info mx-md-auto mb-5 text-center px-4 p-md-0 text-shadow'>Remote Monitoring & Report System</div>
                 <div className='w-100'>
                     <Monitoring img={img2} text={"Multiple Transformar Status"}></Monitoring>
                     <Monitoring1 img={img3} text={"Time Series Transformers Parameter"}></Monitoring1>
--- src/Components/Monitoring/Monitoring.jsx
+++ src/Components/Monitoring/Monitoring.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Monitoring = ({ img, text }) => {
     return (
-        <div className='d-flex flex-column-reverse flex-md-row justify-content-center justify-content-md-between align-items-center my-6' style={{ padding: '50px 50px' }}>
+        <div className='d-flex flex-column-reverse flex-md-row justify-content-center justify-content-md-between align-items-center my-6' style={{ padding: '40px' }}>
             <img data-aos="fade-right" data-aos-duration="2000" className='w-50' src={img} alt="" />
             <div className='mx-auto'>
                 <h1 data-aos="fade-left" data-aos-duration="2000" className='py-3 text-shadow text-center' style={{maxWidth:'400px'}}>{text}</h1>
