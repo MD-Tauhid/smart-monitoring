d27d39192216431842dd0590f62b088fb76d8580
Your Name <tauhid.swe@gmail.com>
2023-11-10 11:56:19 +0600

    responsive issue fixed

--- src/Components/Monitoring/Monitoring1.jsx
+++ src/Components/Monitoring/Monitoring1.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Monitoring1 = ({ img, text, bg, shadow }) => {
     return (
-        <div className={`w-100 d-flex flex-column-reverse flex-md-row-reverse justify-content-center justify-content-md-between align-items-center my-6 ${bg}`} style={{padding:'50px'}}>
-            <img data-aos-duration="2000" className={`w-50 ${shadow}`} src={img} alt="" />
-            <div className='mx-auto'>
-                <h1 data-aos="fade-right" data-aos-duration="2000" className='py-3 text-center text-shadow' style={{maxWidth:'400px'}} >{text}</h1>
+        <div className={`w-100 row my-4 ${bg}`} style={{ padding: '50px' }}>
+            <div className='w-100 col-12 col-md-6 mx-auto my-auto text-center' style={{ maxWidth:'400px'}}>
+                <h1 data-aos="fade-right" data-aos-duration="2000" className='text-shadow text-center w-100 mb-4'>{text}</h1>
+            </div>
+            <div className='col-12 col-md-6 mx-auto w-auto'>
+                <img data-aos="fade-left" data-aos-duration="2000" className={`w-100 col-6 mx-auto ${shadow}`} src={img} alt="" style={{ maxWidth:'550px' }} />
             </div>
         </div>
     );
